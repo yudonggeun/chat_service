28bb5e92526a7abe81604d2d209c9c01f151b6a5
yudonggeun <ydong98@gmail.com>
2023-09-22 12:03:44 +0900

    외부 브로커 환경 구현

--- src/main/java/com/websocket/demo/WebSocketConfig.java
+++ src/main/java/com/websocket/demo/WebSocketConfig.java
@@ -3,10 +3,12 @@ package com.websocket.demo;
 import com.websocket.demo.interceptor.ChatHandshakeInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.session.Session;
 import org.springframework.session.web.socket.config.annotation.AbstractSessionWebSocketMessageBrokerConfigurer;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -16,19 +18,26 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractSessionWebSocketMessageBrokerConfigurer{
 
+    @Value("${spring.rabbitmq.host}")
+    private String host;
     @Autowired
     ChatHandshakeInterceptor chatHandshakeInterceptor;
 
     @Override
     protected void configureStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chatting")
+                .setAllowedOrigins("*")
                 .addInterceptors(chatHandshakeInterceptor);
     }
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+//        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app")
+                .enableStompBrokerRelay("/topic")
+                .setRelayHost(host)
+                .setVirtualHost("/")
+                .setRelayPort(61613);
     }
 
 }
